48fd89ad327f4dbdeedb2e1ccfff849911d1fa04
Thamer HMAIDIm <hmaidithamer@hotmail.com>
2023-06-15 09:36:30 +0100

    feat: 🎸 add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
